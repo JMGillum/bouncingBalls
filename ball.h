d6fc45afe84d8535f77089283350963161dfb638
JMGillum <gillum0927@gmail.com>
2023-11-16 19:01:23 -0800

    Added constructor and destructor definitions, as well as getter functions for position/velocity

--- ball.h
+++ ball.h
@@ -6,9 +6,29 @@
 class Ball : sf::Drawable {
 
     public:
+        Ball();
+        Ball(double radius, sf::Color mColor);
+        ~Ball();
+
+        // Returns information about the object's location
+        sf::Vector2f position() const { return mPosition; }
+        float x() const { return mPosition.x; }
+        float y() const { return mPosition.y; }
+
+        // Returns information about the object's velocity
+        sf::Vector2f velocity() const { return mVelocity; }
+        float velocityX() const { return mVelocity.x; }
+        float velocityY() const { return mVelocity.y; }
+        
 
 
     protected:
+
+        sf::Sprite mSprite;
+        sf::Color mColor;
+        double mRadius;
+        sf::Vector2f mVelocity;
+        sf::Vector2f mPosition;
 
 
     private:
