9410e3ed377b2640e5e4cbbb24d3b27cfa517352
JMGillum <gillum0927@gmail.com>
2023-11-16 19:55:39 -0800

    Implemented constructor

--- ball.cpp
+++ ball.cpp
@@ -1,1 +1,32 @@
 #include "ball.h"
+
+
+
+Ball::Ball(sf::Vector2f position, double radius, sf::Color color){
+    if(radius > 0){
+        mRadius = radius;
+    } else {
+        mRadius = 0;
+    }
+
+    mPosition = position;
+    mColor = color;
+
+    mVelocity = sf::Vector2f(0,0);
+}
+
+
+
+Ball::Ball(const Ball &right){
+
+
+}
+
+
+
+Ball::~Ball(){
+
+
+}
+
+
