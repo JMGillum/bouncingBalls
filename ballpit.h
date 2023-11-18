9c208d738cdaa6820578468ba90d921d6e5db610
JMGillum <gillum0927@gmail.com>
2023-11-17 20:35:26 -0800

    Added ballpit class and constructors and draw function

---
+++ ballpit.h
@@ -0,0 +1,32 @@
+#ifndef BALLPIT_H
+#define BALLPIT_H
+
+#include <vector>
+
+#include "ball.h"
+
+class Ballpit {
+
+    public:
+
+        Ballpit() : Ballpit(20) {};
+        Ballpit(unsigned numBalls);
+        ~Ballpit();
+
+
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    protected:
+
+        std::vector<Ball*> mBalls;
+        unsigned mNumBalls;
+
+
+    private:
+
+};
+
+
+
+
+#endif
