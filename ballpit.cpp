78dab44554535438d54778b2a16802090e893183
JMGillum <gillum0927@gmail.com>
2023-11-24 12:01:56 -0800

    Implemented checkCollision functions ~ untested

--- ballpit.cpp
+++ ballpit.cpp
@@ -17,9 +17,11 @@ Ballpit::Ballpit(unsigned numBalls){
 
     Ball* ptr = nullptr;
     for(unsigned i = 0; i < mNumBalls; ++i){
-        ptr = new Ball(sf::Vector2f(200*(i+1),200),40,sf::Color::Green);
+        ptr = new Ball(sf::Vector2f(200*(i+1),200),sf::Vector2f(1,3),40,sf::Color::Green);
         mBalls.push_back(ptr);
     }
+
+    SCREEN_SIZE = sf::Vector2f(-1,-1);
 
 }
 
@@ -35,6 +37,20 @@ Ballpit::~Ballpit(){
 }
 
 
+void Ballpit::checkCollision(){
+    for(unsigned i = 0; i < mNumBalls - 1; i++){
+        if(mBalls[i] != nullptr){
+            for(unsigned j = i+1; j < mNumBalls; ++j){
+                if(mBalls[j] != nullptr){
+                    checkCollision(mBalls[i],mBalls[j]);
+                }
+            }
+            checkCollisionWalls(mBalls[i]);
+        } 
+    }
+}
+
+
 /**
  * @brief Checks the collision betwen two balls
  * 
@@ -43,18 +59,18 @@ Ballpit::~Ballpit(){
  * @return true, they are colliding
  * @return false, they are not colliding
  */
-bool Ballpit::checkCollision(const Ball& ballOne, const Ball& ballTwo){
+bool Ballpit::checkCollision(const Ball* ballOne, const Ball* ballTwo){
     double distance = 0;
     double maxDistance = 0;
 
-    maxDistance = ballOne.radius() + ballTwo.radius();
+    maxDistance = ballOne->radius() + ballTwo->radius();
 
     double x = 0, y = 0;
 
-    x = ballOne.x() - ballTwo.x();
+    x = ballOne->x() - ballTwo->x();
     if(x < 0) { x *= -1; }
 
-    y = ballOne.y() - ballTwo.y();
+    y = ballOne->y() - ballTwo->y();
     if(y < 0) { y *= -1; }
 
     distance = (x * x) + (y * y);
@@ -63,6 +79,26 @@ bool Ballpit::checkCollision(const Ball& ballOne, const Ball& ballTwo){
 
     if(distance > maxDistance) { return false; }
     return true;
+}
+
+bool Ballpit::checkCollisionWalls(const Ball* ball){
+    if(SCREEN_SIZE.x < ball->radius() || SCREEN_SIZE.y < ball->radius()){
+        throw ScreenSizeNotSpecified();
+    }
+    if((ball->x() + ball->radius()) > SCREEN_SIZE.x){
+        return true;
+    }
+    if((ball->x() - ball->radius()) < 0){
+        return true;
+    }
+    if((ball->y() + ball->radius()) > SCREEN_SIZE.y){
+        return true;
+    }
+    if((ball->y() + ball->radius()) < 0){
+        return true;
+    }
+
+    return false;
 }
 
 
--- ballpit.h
+++ ballpit.h
@@ -25,16 +25,24 @@ class Ballpit : public sf::Drawable{
         Ballpit(unsigned numBalls);
         ~Ballpit();
 
+        void checkCollision();
        
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const; // Draws the balls
 
         void move();
 
+        void setScreenSize(sf::Vector2f size) { SCREEN_SIZE = size; }
+
+        class ScreenSizeNotSpecified {};
+
     protected:
 
         std::vector<Ball*> mBalls; // Stores the balls
         unsigned mNumBalls; // The number of balls in the ballpit
-        bool checkCollision(const Ball& ballOne, const Ball& ballTwo); // Checkes collision between every ball
+        bool checkCollision(const Ball* ballOne, const Ball* ballTwo); // Checkes collision between every ball
+        bool checkCollisionWalls(const Ball* ball);
+
+        sf::Vector2f SCREEN_SIZE;
 
 
 
